74a39fa70306d0c2d5f7766d68a59e881c4023be
Fernando Sandoval <fmsandoval@uc.cl>
2018-03-19 01:18:14 -0300

    mejoras, link a heroku y seed basico

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(email: 'fmsandoval@uc.cl', password: 'iic3103t1fmsandoval')
+Post.create(title: 'Titular 1.', lead: 'Bajada de la noticia 1.', body: 'Cuerpo de la noticia 1.')
+Post.create(title: 'Titular 2.', lead: 'Bajada de la noticia 2.', body: 'Cuerpo de la noticia 2. Otra linea para que sea un poco mas largo.')
+Post.create(title: 'Titular 3.', lead: 'Bajada de la noticia 3.', body: 'Cuerpo de la noticia 3.')
+Comment.create(commenter: 'Comentador 1', body: 'Cuerpo del comentario 1.', post_id: 1)
